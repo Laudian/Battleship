57466d96b909bf115aa787ca9b24883963d06f44
mrabe <mrabe@uni-bremen.de>
2022-06-05 21:34:01 +0200

    Game in playable state in console, no errorhandling

--- BattleshipGame.py
+++ BattleshipGame.py
@@ -1,27 +1,51 @@
 from battleshipboard import *
 
-ships = [Ship(1, 1, Orientation.HORIZONTAL, 2),
-         Ship(3, 3, Orientation.VERTICAL, 3)]
-board = BattleshipBoard(ships, 10)
+ships0 = [Ship(1, 1, Orientation.HORIZONTAL, 2),
+          Ship(3, 3, Orientation.VERTICAL, 3)]
 
-
-def move(x, y):
-    result = board.move((x, y))
-    print(result)
-
-
-move(0, 0)
-move(1, 1)
-move(1, 2)
-move(2, 1)
-move(3, 3)
-move(3, 4)
-move(3, 5)
+ships1 = [Ship(1, 1, Orientation.HORIZONTAL, 3),
+          Ship(3, 3, Orientation.VERTICAL, 2)]
 
 
 class BattleshipGame(object):
-    def __init__(self, size, ships1, ships2):
+    def __init__(self, size, ships0, ships1, rule_move_until_miss: bool = True):
+        player0_board = BattleshipBoard(ships0, size)
         player1_board = BattleshipBoard(ships1, size)
-        player2_board = BattleshipBoard(ships2, size)
-        player1_history = BattleshipBoard([], size)
-        player2_history = BattleshipBoard([], size)
+        self.boards = [player0_board, player1_board]
+        self.turn = 0
+        self.rule_move_until_miss = rule_move_until_miss
+
+    def move(self, x: int, y: int) -> (BSMoveResult, int):
+        if self.turn == 2:
+            raise Exception("Cannot make move because game has finished already")
+        result = self.boards[self.turn].move((x, y))
+        if result == BSMoveResult.MISS:
+            self.turn = not self.turn
+        if result in [BSMoveResult.HIT, BSMoveResult.HIT_AND_SUNK]:
+            if not self.rule_move_until_miss:
+                self.turn = not self.turn
+        if result == BSMoveResult.END:
+            self.turn = 2
+        return result, self.turn
+
+    def getactiveplayer(self):
+        # returns either the index of the active player or 2 if the game is over
+        return int(self.turn)
+
+    def displaygamestate(self):
+        print("Player 0:")
+        self.boards[0].display()
+        print("Player 1:")
+        self.boards[1].display()
+
+
+if __name__ == "__main__":
+    game = BattleshipGame(12, ships0, ships1)
+    result, player_to_move = game.move(0, 0)
+    result, player_to_move = game.move(1, 1)
+    result, player_to_move = game.move(1, 2)
+    result, player_to_move = game.move(2, 1)
+    result, player_to_move = game.move(3, 3)
+    result, player_to_move = game.move(3, 4)
+    result, player_to_move = game.move(3, 5)
+    game.displaygamestate()
--- battleshipboard.py
+++ battleshipboard.py
@@ -6,7 +6,7 @@ class Orientation(Enum):
     HORIZONTAL = 2
 
 
-class BSBoardResult(Enum):
+class BSMoveResult(Enum):
     OUT_OF_BOUNDS = 0
     REPEAT_SHOT = 1
     MISS = 2
@@ -41,52 +41,76 @@ class Ship(object):
     def register(self, board: "BattleshipBoard"):
         self.board = board
         for square in self.squares:
-            board.board[square] = self
+            board.grid[square] = self
 
     def hit(self, square):
         self.size -= 1
         if self.size == 0:
             for square in self.squares:
-                self.board.board[square] = BSBoardState.SUNK
+                self.board.grid[square] = BSBoardState.SUNK
             self.board.ships.remove(self)
-            return BSBoardResult.HIT_AND_SUNK
+            return BSMoveResult.HIT_AND_SUNK
         else:
-            self.board.board[square] = BSBoardState.HIT
-            return BSBoardResult.HIT
+            self.board.grid[square] = BSBoardState.HIT
+            return BSMoveResult.HIT
 
 
 class BattleshipBoard(object):
     def __init__(self, ships, size):
         self.size = size
-        self.board = dict()
+        self.grid = dict()
         self.ships = ships
 
-        for x in range(1, size+1):
-            for y in range(1, size + 1):
-                self.board[(x, y)] = BSBoardState.WATER
+        for x in range(size):
+            for y in range(size):
+                self.grid[(x, y)] = BSBoardState.WATER
 
         for ship in ships:
             ship.register(self)
         return
 
     def mark(self, square, marker):
-        self.board[square] = marker
+        self.grid[square] = marker
 
-    def move(self, square):
-        current = self.board.get(square, BSBoardState.OUT_OF_BOUNDS)
+    @staticmethod
+    def state_to_char(state: BSBoardState) -> str:
+        if state == BSBoardState.WATER:
+            return " "
+        elif state == BSBoardState.HIT:
+            return "X"
+        elif state == BSBoardState.SUNK:
+            return "#"
+        elif state == BSBoardState.MISS:
+            return "O"
+        elif isinstance(state, Ship):
+            return "S"
+        else:
+            raise ValueError(f"State {state} has no representation.")
+
+    def move(self, square) -> BSMoveResult:
+        current = self.grid.get(square, BSBoardState.OUT_OF_BOUNDS)
         if current == BSBoardState.OUT_OF_BOUNDS:
-            return BSBoardResult.OUT_OF_BOUNDS
+            return BSMoveResult.OUT_OF_BOUNDS
         elif current in (BSBoardState.HIT, BSBoardState.MISS, BSBoardState.SUNK):
-            return BSBoardResult.REPEAT_SHOT
+            return BSMoveResult.REPEAT_SHOT
         elif current == BSBoardState.WATER:
-            self.board[square] = BSBoardState.MISS
-            return BSBoardResult.MISS
+            self.grid[square] = BSBoardState.MISS
+            return BSMoveResult.MISS
         elif isinstance(current, Ship):
             result = current.hit(square)
-            if result == BSBoardResult.HIT:
+            if result == BSMoveResult.HIT:
                 return result
-            elif result == BSBoardResult.HIT_AND_SUNK:
+            elif result == BSMoveResult.HIT_AND_SUNK:
                 if self.ships:
                     return result
                 else:
-                    return BSBoardResult.END
+                    return BSMoveResult.END
+
+    def display(self):
+        print((2 * self.size + 1) * "_")
+        for y in range(self.size):
+            rowstring: str = "|"
+            for x in range(self.size):
+                rowstring += self.state_to_char(self.grid[(x, y)]) + "|"
+            print(rowstring)
+        print((2 * self.size + 1) * "_")
